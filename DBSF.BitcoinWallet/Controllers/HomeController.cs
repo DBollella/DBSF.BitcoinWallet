16f4149c8956f1fbcbdad000ca1e7402c3027390
DBollella <douglas.bollella@gmail.com>
2018-08-21 23:46:14 -0300

    Initial Commit

---
+++ DBSF.BitcoinWallet/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBSF.BitcoinWallet.Infra.External;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DBSF.BitcoinWallet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private MercadoBitcoin objMercadoBitcoin;
+
+        public HomeController()
+        {
+            //TODO: Implementar Injeção Dependencia
+            objMercadoBitcoin = new MercadoBitcoin();
+        }
+
+
+        [HttpGet]
+        public ActionResult<string> ResumoDiario()
+        {
+            var retorno = string.Empty;
+            try
+            {
+                retorno = objMercadoBitcoin.ResumoDiario().Result.ToString();   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex);
+            }
+            return retorno;
+        }   
+    }
+}
