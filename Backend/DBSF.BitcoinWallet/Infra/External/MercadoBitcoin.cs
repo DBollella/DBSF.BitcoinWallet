ca5c4f45ff2a97c79533c2cf5768b2d5a212876f
DBollella <douglas.bollella@gmail.com>
2018-08-21 23:51:38 -0300

    estruturacao

---
+++ Backend/DBSF.BitcoinWallet/Infra/External/MercadoBitcoin.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DBSF.BitcoinWallet.Infra.External
+{
+    public class MercadoBitcoin
+    {
+        static HttpClient client;
+
+        public MercadoBitcoin()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<object> ResumoDiario()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://www.mercadobitcoin.net/api/BTC/ticker/");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();           
+            return responseBody;            
+        }
+    }
+}
